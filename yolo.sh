a298384e537b648764b1c950e2f2ed17d6432c26
yuenhern96@gmail.com <yuenhern96@gmail.com>
2023-01-09 21:41:50 +0800

    Add bash and submit file for HTCondor use case

---
+++ yolo.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+# file name: yolo_test.sh
+
+# check python version
+echo "$(python3 --version)"
+
+# change to directory
+cd /home/ubuntu/yolo/um-wqd7008-pdc-yolov5
+echo "Directory changed"
+
+# activate python env
+source venv/bin/activate
+echo "Python env activated"
+
+# run detect.py on an image
+echo "Execution start"
+python3 detect.py --weights yolov5s.pt --source data/images/bus.jpg
+echo "Execution complete"
+
+# deactivate python env
+deactivate
+echo "Python env deactivated"
